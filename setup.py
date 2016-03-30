951448a856620bed6def67cd772c00b399408c7c
Bartosz Zaczyński <bartosz.zaczynski@gmail.com>
2016-03-30 21:19:25 +0200

    Fix setup script to use setuptools instead of distutils

--- setup.py
+++ setup.py
@@ -1,22 +1,54 @@
 #!/usr/bin/env python
 
-from distutils.core import setup
+# The MIT License (MIT)
+#
+# Copyright (c) 2013 Bartosz Zaczynski
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
+
+import os.path
+
+from setuptools import setup, find_packages
 
 
-def get_readme_content():
-    with open('README.md') as file_handle:
-        return file_handle.read()
+def path(*parts):
+    """Return an absolute path to the given resource."""
+    dirname = os.path.dirname(__file__)
+    return os.path.join(dirname, *parts)
 
 
-setup(name='microanalyst',
-      version='1.0.0',
-      author='Bartosz Zaczynski',
-      author_email='bartosz.zaczynski@gmail.com',
+def find_version():
+    """Return the value of gpcommons.__version__."""
+    with open(path('microanalyst', '__init__.py')) as fp:
+        exec fp
+    return locals().get('__version__')
+
+
+setup(name=u'microanalyst',
+      version=find_version(),
+      author=u'Bartosz Zaczy\u0144ski',
+      author_email=u'bartosz.zaczynski@gmail.com',
       url='https://github.com/bzaczynski/microanalyst',
       download_url='https://github.com/bzaczynski/microanalyst.git',
       description='Tecan(R) i-control(TM) Data Analysis',
-      long_description=get_readme_content(),
-      platforms = ['Any'],
+      long_description=open(path('README.md')).read(),
+      platforms=['Any'],
       keywords=['microbiology', 'microplate', 'microtiter', 'tecan'],
       license='MIT License',
       classifiers=(
@@ -31,25 +63,19 @@ setup(name='microanalyst',
           'Topic :: Scientific/Engineering :: Bio-Informatics',
           'Topic :: Scientific/Engineering :: Visualization'
       ),
-      requires=[
-          'xlwt',
-          'xlrd',
-          'numpy'
-      ],
-      packages=[
-          'microanalyst',
-          'microanalyst.commons',
-          'microanalyst.gui',
-          'microanalyst.model',
-          'microanalyst.xls'
-      ],
+      dependency_links=[],
+      install_requires=open(path('requirements.txt')).read().splitlines(),
+      tests_require=[],
+      test_suite='tests',
+      packages=find_packages(exclude=['tests']),
+      package_data={},
       scripts=[
-        'scripts/group.py',
-        'scripts/control.py',
-        'scripts/genes.py',
-        'scripts/assemble.py',
-        'scripts/quantize.py',
-        'scripts/xlsh.py',
-        'scripts/xlsv.py',
-        'scripts/manalyst.pyw'
+          'scripts/group.py',
+          'scripts/control.py',
+          'scripts/genes.py',
+          'scripts/assemble.py',
+          'scripts/quantize.py',
+          'scripts/xlsh.py',
+          'scripts/xlsv.py',
+          'scripts/manalyst.pyw'
       ])
